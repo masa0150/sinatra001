8298dadb464cf79b09063ee7edf621247cbd0589
masa0150 <masa98989801@yahoo.co.jp>
2020-02-07 12:41:43 +0900

    sample.sinatra

---
+++ app.rb
@@ -0,0 +1,28 @@
+require 'sinatra'
+require 'sinatra/reloader'
+
+get '/' do
+  erb :index  
+end
+
+get '/about' do
+  erb :about
+end
+
+get '/shop' do
+  erb :shop
+end
+
+get '/info' do
+  erb :info
+end
+
+get '/accses' do
+  erb :accses
+end
+
+post '/confirm' do
+  @email = params[:email]
+  @message = params[:message]
+  erb :confirm
+end
